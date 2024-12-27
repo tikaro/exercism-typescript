292d9f9e87e207b1d5fb58d3a859336a4f6f34e5
John Young <john@tikaro.net>
2024-12-27 21:14:57 +0000

    copy change to leap.ts

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -3,12 +3,12 @@ export function isLeap(year: number): boolean {
   // a year that is divisible by 100 and also by 400 is a leap year
   if(year % 100 === 0 && year % 400 === 0) { return true };
 
-  // but if it's not divisible by 400, it's not a leap year
+  // but if it's not also divisible by 400, it's not a leap year
   if(year % 100 === 0) { return  false }
 
   // a year that is divisible by 4 is a leap year
   if(year % 4 === 0) { return true };
 
-  // if it didn't pass either of those conditions, it's not a leap year
+  // if we made it this far, it's not a leap year
   return false;
 }
