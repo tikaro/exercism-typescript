24a6119f0b104468f0be9519af5ea98672eece76
John Young <john@tikaro.net>
2024-12-27 21:14:18 +0000

    Add solution to Leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,14 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number): boolean {
+
+  // a year that is divisible by 100 and also by 400 is a leap year
+  if(year % 100 === 0 && year % 400 === 0) { return true };
+
+  // but if it's not divisible by 400, it's not a leap year
+  if(year % 100 === 0) { return  false }
+
+  // a year that is divisible by 4 is a leap year
+  if(year % 4 === 0) { return true };
+
+  // if it didn't pass either of those conditions, it's not a leap year
+  return false;
 }
